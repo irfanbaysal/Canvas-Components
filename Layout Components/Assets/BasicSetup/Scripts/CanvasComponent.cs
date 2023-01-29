62d1c5bf82b7b9559be0085f913d6d0d43ecae35
İrfan Yiğit Baysal <irfanyigitbaysal@rubygamestudio.com>
2023-01-29 15:53:38 +0300

    CanvasGroupComponent added

---
+++ Layout Components/Assets/BasicSetup/Scripts/CanvasComponent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core.Scripts.UI
+{
+    public class CanvasComponent : UIBehaviour
+    {
+
+    }
+}
+
---
+++ Layout Components/Assets/BasicSetup/Scripts/CanvasGroupComponent.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace Core.Scripts.UI
+{
+    [Flags]
+     public enum CanvasGroupFlags
+     {
+         Interactable = 1 << 1,
+         BlockRaycast = 1 << 2,
+         IgnoreParent = 1 << 3,
+     }
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupComponent : UIBehaviour
+    {
+        [SerializeField] private CanvasGroupFlags properties;
+        [SerializeField] private bool enableCanvasGroup=true;
+        [SerializeField][Range(0,1)] private float alpha;
+        private bool Interactable => properties.HasFlag(CanvasGroupFlags.Interactable);
+        private bool BlockRaycast => properties.HasFlag(CanvasGroupFlags.BlockRaycast);
+        private bool IgnoreParent => properties.HasFlag(CanvasGroupFlags.IgnoreParent);
+        public CanvasGroup CanvasGroup => _canvasGroup ? _canvasGroup : (_canvasGroup = GetComponent<CanvasGroup>());
+        private CanvasGroup _canvasGroup;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            SetAlpha();
+            SetInteractable();
+            SetBlockRaycasts();
+            SetIgnoreParentGroup();
+            EnableCanvasGroup();
+        }
+        
+        public void EnableCanvasGroup()
+        {
+            CanvasGroup.enabled = enableCanvasGroup;
+        }
+        
+        public void SetAlpha()
+        {
+            CanvasGroup.alpha = alpha;
+        }
+
+        public void SetInteractable()
+        {
+            CanvasGroup.interactable = Interactable;
+        }
+        
+        public void SetIgnoreParentGroup()
+        {
+            CanvasGroup.ignoreParentGroups = IgnoreParent;
+        }
+
+        public void SetBlockRaycasts()
+        {
+            CanvasGroup.blocksRaycasts = BlockRaycast;
+        }   
+    }
+}
+
