5cf032a6c355dd9832100dc23759e1e49e35b15c
İrfan Yiğit Baysal <irfanyigitbaysal@rubygamestudio.com>
2023-01-29 14:37:03 +0300

    Small additions

--- Layout Components/Assets/BasicSetup/Scripts/LayoutHandlerComponents/LayoutComponent.cs
+++ Layout Components/Assets/BasicSetup/Scripts/LayoutHandlerComponents/LayoutComponent.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Core.Scripts.UI
@@ -13,24 +15,35 @@ namespace Core.Scripts.UI
         ForceRebuildLayoutImmediate
     }
 
-    [RequireComponent(typeof(ContentSizeFitter),typeof(CanvasGroup))]
+
+    [RequireComponent(typeof(ContentSizeFitter), typeof(CanvasGroup))]
     [DisallowMultipleComponent]
     public class LayoutComponent : UIBehaviour
     {
+        
+        [SerializeField] private bool isEnableOnStart = true;
         [SerializeField] protected BuilderType builderType;
-        protected LayoutGroup LayoutGroup; 
+        [SerializeField] protected bool useForceCanvases = true;
+        protected HorizontalOrVerticalLayoutGroup LayoutGroup;
         protected RectTransform RectTransform;
-        protected CanvasGroup CanvasGroup; 
+        protected CanvasGroup CanvasGroup;
         protected ContentSizeFitter ContentSizeFitter => GetComponent<ContentSizeFitter>();
-       
+        
         protected override void Awake()
         {
             base.Awake();
-            LayoutGroup = GetComponent<LayoutGroup>();
+            LayoutGroup = GetComponent<HorizontalOrVerticalLayoutGroup>();
             RectTransform = GetComponent<RectTransform>();
             CanvasGroup = GetComponent<CanvasGroup>();
+            EnableLayoutGroup(isEnableOnStart);
         }
- 
+
+        protected override void OnRectTransformDimensionsChange()
+        {
+            base.OnRectTransformDimensionsChange();
+            ForceRebuilder();
+        }
+
         protected void SetBuilderType(BuilderType type)
         {
             this.builderType = type;
@@ -40,44 +53,49 @@ namespace Core.Scripts.UI
         {
             if (type == BuilderType.MarkLayoutForRebuild)
             {
-                await UniTask.Yield(cancellationToken:this.GetCancellationTokenOnDestroy());
+                await UniTask.Yield(cancellationToken: this.GetCancellationTokenOnDestroy());
                 LayoutRebuilder.MarkLayoutForRebuild(RectTransform);
             }
             else
                 LayoutRebuilder.ForceRebuildLayoutImmediate(RectTransform);
-            
+
             ForceUpdateCanvas();
+            
+            Debug.Log("Dimension Changed");
         }
+
         protected void EnableLayoutGroup(bool isEnabled)
         {
             LayoutGroup.enabled = isEnabled;
         }
 
-        protected void SetCanvasGroupAlpha(float? alpha=null)
+        protected void SetCanvasGroupAlpha(float? alpha = null)
         {
             CanvasGroup.alpha = alpha ?? 1f;
         }
 
-        protected void SetCanvasGroupInteractable(bool isInteractable=true)
+        protected void SetCanvasGroupInteractable(bool isInteractable = true)
         {
             CanvasGroup.interactable = isInteractable;
         }
-        
-        protected void SetCanvasGroupBlockRaycasts(bool blocksRaycast=true)
+
+        protected void SetCanvasGroupBlockRaycasts(bool blocksRaycast = true)
         {
             CanvasGroup.blocksRaycasts = blocksRaycast;
         }
-        
+
         private async void SetLastSibling(RectTransform rectTransform)
         {
-            await UniTask.Yield(cancellationToken:this.GetCancellationTokenOnDestroy());
-            LayoutRebuilder.MarkLayoutForRebuild(rectTransform); 
+            await UniTask.Yield(cancellationToken: this.GetCancellationTokenOnDestroy());
+            LayoutRebuilder.MarkLayoutForRebuild(rectTransform);
             Canvas.ForceUpdateCanvases();
         }
+
         protected void ForceUpdateCanvas()
         {
-            Canvas.ForceUpdateCanvases();   
+            if(!useForceCanvases)return;
+            Canvas.ForceUpdateCanvases();
         }
-        
+
     }
 }
